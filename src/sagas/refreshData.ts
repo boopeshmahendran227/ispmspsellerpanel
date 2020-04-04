acfec9783fafdf9708fefb64dd5f25cb3b592147
Boopesh Mahendran <boopeshmahendran@Boopeshs-MacBook-Pro.local>
2020-04-04 20:32:46 +0530

    Fix status change bug

--- src/sagas/refreshData.ts
+++ src/sagas/refreshData.ts
@@ -1,10 +1,10 @@
-import { CHANGE_ORDER_ITEM_STATUS_REQUEST } from "../constants/ActionTypes";
+import { CHANGE_ORDER_ITEM_STATUS_SUCCESS } from "../constants/ActionTypes";
 import { take, all, put } from "redux-saga/effects";
 import OrderActions from "../actions/order";
 
 function* refreshOrder() {
   while (true) {
-    yield take([CHANGE_ORDER_ITEM_STATUS_REQUEST]);
+    yield take([CHANGE_ORDER_ITEM_STATUS_SUCCESS]);
     yield put(OrderActions.getCurrentOrder());
   }
 }
