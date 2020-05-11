ec9410e57bc4c82c6abba3d4a20a785998af3dae
Boopesh Mahendran <boopesh@cybermindworks.com>
2020-05-11 16:56:59 +0530

    Change next js config

--- next.config.js
+++ next.config.js
@@ -1,40 +1,38 @@
-let publicRuntimeConfig = null;
-
-switch (process.env.ENV) {
-  case "local":
-    publicRuntimeConfig = {
-      marketPlaceApiUrl: "https://mplapidev.istakapaza.com/v1",
-      istakapazaApiUrl: "https://ispdev.istakapaza.com",
-      notificationApiUrl: "https://mplnotifydev.istakapaza.com",
-      cdnUrl: "https://mpldev.cdn.istakapaza.com",
-    };
-    break;
-  case "dev":
-    publicRuntimeConfig = {
-      marketPlaceApiUrl: "https://mplapidev.istakapaza.com/v1",
-      istakapazaApiUrl: "https://ispdev.istakapaza.com",
-      notificationApiUrl: "https://mplnotifydev.istakapaza.com",
-      cdnUrl: "https://mpldev.cdn.istakapaza.com",
-    };
-    break;
-  case "stage":
-    publicRuntimeConfig = {
-      marketPlaceApiUrl: "https://mplapisit.istakapaza.com/v1",
-      istakapazaApiUrl: "https://test.istakapaza.com",
-      notificationApiUrl: "https://mplnotifysit.istakapaza.com",
-      cdnUrl: "https://mpldev.cdn.istakapaza.com",
-    };
-    break;
-  case "prod":
-    publicRuntimeConfig = {
-      marketPlaceApiUrl: "https://mplapi.istakapaza.com/v1",
-      istakapazaApiUrl: "https://istakapaza.com",
-      notificationApiUrl: "https://mplnotify.istakapaza.com",
-      cdnUrl: "https://mpldev.cdn.istakapaza.com",
-    };
-    break;
-}
-
-module.exports = {
-  publicRuntimeConfig,
+const getConfig = () => {
+  switch (process.env.ENV) {
+    case "local":
+      return {
+        marketPlaceApiUrl: "https://mplapidev.istakapaza.com/v1",
+        istakapazaApiUrl: "https://ispdev.istakapaza.com",
+        notificationApiUrl: "https://mplnotifydev.istakapaza.com",
+        cdnUrl: "https://mpldev.cdn.istakapaza.com",
+      };
+    case "dev":
+      return {
+        marketPlaceApiUrl: "https://mplapidev.istakapaza.com/v1",
+        istakapazaApiUrl: "https://ispdev.istakapaza.com",
+        notificationApiUrl: "https://mplnotifydev.istakapaza.com",
+        cdnUrl: "https://mpldev.cdn.istakapaza.com",
+      };
+    case "stage":
+      return {
+        marketPlaceApiUrl: "https://mplapisit.istakapaza.com/v1",
+        istakapazaApiUrl: "https://test.istakapaza.com",
+        notificationApiUrl: "https://mplnotifysit.istakapaza.com",
+        cdnUrl: "https://mpldev.cdn.istakapaza.com",
+      };
+    case "prod":
+      return {
+        marketPlaceApiUrl: "https://mplapi.istakapaza.com/v1",
+        istakapazaApiUrl: "https://istakapaza.com",
+        notificationApiUrl: "https://mplnotify.istakapaza.com",
+        cdnUrl: "https://ispmpl.cdn.istakapaza.com",
+      };
+  }
 };
+
+const withImages = require("next-images");
+
+module.exports = withImages({
+  publicRuntimeConfig: getConfig(),
+});
