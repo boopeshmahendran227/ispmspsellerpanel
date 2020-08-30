7b41072548f47746a71280eb66965020290b7d02
Boopesh Mahendran <boopesh@cybermindworks.com>
2020-08-30 16:42:23 +0530

    fix things

--- src/components/RevenueLineChart.tsx
+++ src/components/RevenueLineChart.tsx
@@ -1,6 +1,7 @@
 import { ResponsiveLine } from "@nivo/line";
 import moment from "moment";
 import CSSConstants from "../constants/CSSConstants";
+import { formatPrice } from "utils/misc";
 
 const formatLineData = (data) => {
   const monthWiseRevenueList = data.map((object) => ({
@@ -40,7 +41,7 @@ const RevenueLineChart = (props: RevenueDataProps) => (
       tickSize: 2,
       tickPadding: 5,
       tickRotation: 0,
-      legend: "Period",
+      legend: "Month",
       legendOffset: 36,
       legendPosition: "middle",
     }}
@@ -61,8 +62,9 @@ const RevenueLineChart = (props: RevenueDataProps) => (
         9 * props.interval,
       ],
       legend: "Revenue",
-      legendOffset: -70,
+      legendOffset: -80,
       legendPosition: "middle",
+      format: (value) => formatPrice(Number(value)),
     }}
     colors={[CSSConstants.secondaryColor]}
     enableGridX={false}
@@ -76,8 +78,33 @@ const RevenueLineChart = (props: RevenueDataProps) => (
     enablePoints={true}
     enableArea={true}
     areaBaselineValue={0}
-    areaOpacity={0.3}
+    areaOpacity={0.2}
     useMesh={true}
+    enableSlices="y"
+    sliceTooltip={({ slice }) => {
+      return (
+        <div
+          style={{
+            background: "white",
+            padding: "9px 12px",
+            border: "1px solid #f0f0f0",
+            boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+          }}
+        >
+          {slice.points.slice(0, 1).map((point) => (
+            <div>
+              <div>
+                <strong>Month:</strong> {point.data.xFormatted}
+              </div>
+              <div key={point.id}>
+                <strong>Revenue:</strong>{" "}
+                {formatPrice(Number(point.data.yFormatted))}
+              </div>
+            </div>
+          ))}
+        </div>
+      );
+    }}
   />
 );
 
